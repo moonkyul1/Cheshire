ac84cd95541f043074bd3bea7714493ebad4ea1d
moonkyul1 <moonkyul1@kaist.ac.kr>
2019-05-13 03:16:59 +0900

    like complete

--- screen/feed/feed/feed.js
+++ screen/feed/feed/feed.js
@@ -17,13 +17,25 @@ var con;
 function like(Obj){
     var img1=$(Obj).attr("src")
     con=img1;
+    var akey= $(Obj).parent().attr("id");
+    var user = firebase.auth().currentUser;
     if(img1.indexOf('_selected')==-1){
         img1=img1.replace('.png','_selected.png');
-        $(Obj).attr("src",img1)
+        $(Obj).attr("src",img1);
+        
+        var akey= $(Obj).parent().attr("id");
+        //console.log(user.uid);
+        var newKey = firebase.database().ref('/user/'+user.uid+'/like/'+akey+"/").push();
+        newKey.set({
+          postnum: akey
+        });
+        //del
     }
     else{
         img1=img1.replace('_selected.png','.png');
         $(Obj).attr("src",img1)
+        firebase.database().ref('/user/' +user.uid+'/like/'+akey+'/').remove();
+        //push
     }
 }
  
@@ -32,7 +44,7 @@ var feedstring="<div class=\"feed\">\
 <div class=header>\
 <div class=\"content\" id="+altkey+" ><img class=\"img\" src="+img+" onclick=\"save(this); location.href=\'../../profile/cat/catprofile.html\';\"></div>\
 <div class=\"name\">"+name+"</div>\
-<div class=\"content\" id="+altkey+" ><img class=\"archive\" src=../../../image/icon/bookmark.png onclick=\"picarchive(this)\"></div>\
+<div class=\"content\" id="+altkey+" ><img class=\"archive\" src=../../../image/icon/bookmark.png onclick=\"picarchive(this);alertTestFn();\"></div>\
 </div>\
 <div class=\"content\" id="+altkey+" ><img id=\"contentid\" src="+picture+" onclick=\"save(this); location.href=\'../post/post.html\';\"></div>\
 <div class=\"accessory\" id="+altkey+">\
@@ -43,6 +55,24 @@ var feedstring="<div class=\"feed\">\
 </div>";
 return feedstring;
 }
+
+function makelikefeed(img,name,picture,altkey){
+var feedstring="<div class=\"feed\">\
+<div class=header>\
+<div class=\"content\" id="+altkey+" ><img class=\"img\" src="+img+" onclick=\"save(this); location.href=\'../../profile/cat/catprofile.html\';\"></div>\
+<div class=\"name\">"+name+"</div>\
+<div class=\"content\" id="+altkey+" ><img class=\"archive\" src=../../../image/icon/bookmark.png onclick=\"picarchive(this);alertTestFn();\"></div>\
+</div>\
+<div class=\"content\" id="+altkey+" ><img id=\"contentid\" src="+picture+" onclick=\"save(this); location.href=\'../post/post.html\';\"></div>\
+<div class=\"accessory\" id="+altkey+">\
+<img id=\"comment\" src=../../../image/icon/message.png  onclick=\"save(this); location.href=\'../post/post.html\';\">\
+<img id=\"like\" src=../../../image/icon/heart_selected.png onclick=\"like(this);piclike(this);\">\
+<img id=\"feedgift\" src=../../../image/icon/cf.png onclick=\"location.href=\'../../gift/reward/reward.html\'\">\
+</div>\
+</div>";
+return feedstring;
+}
+
 
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -56,12 +86,37 @@ return feedstring;
     return firebase.database().ref('/post/').once('value',function(snapshot){
       var myValue = snapshot.val();
       var keyList = Object.keys(myValue);
+      console.log(likelist)
       for (var i=num;i<num+1;i++){
         var currentKey = keyList[i];
-        $('#container').append(makefeed(myValue[currentKey].img, myValue[currentKey].name, myValue[currentKey].picture, currentKey));
+        console.log(currentKey)
+        if(likelist.indexOf(currentKey) != -1){
+          $('#container').append(makelikefeed(myValue[currentKey].img, myValue[currentKey].name, myValue[currentKey].picture, currentKey));
+        }
+        else{
+          $('#container').append(makefeed(myValue[currentKey].img, myValue[currentKey].name, myValue[currentKey].picture, currentKey));
+        }
+        
       }
     });
   }
+
+  function readFromLike(user,likelist) {
+    /*
+       Read comments from the database
+       Print all the comments to the table
+    */
+    return firebase.database().ref('/user/'+user.uid+'/like/').once('value',function(snapshot){
+      var myValue = snapshot.val();
+      var keyList = Object.keys(myValue);
+      for (var i=0;i<keyList.length;i++){
+        var currentKey = keyList[i];
+        likelist.push(currentKey);
+      }
+    });
+  }
+
+
 
   function feedgo(num){
     for(var i = 0 ; i<num; i++){
@@ -70,8 +125,24 @@ return feedstring;
     }
   }
 
+  var likelist=[]
 
-feedgo(10);
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      //var imglist=[];
+      readFromLike(user,likelist).then(
+        function(){
+          feedgo(10);  
+        }
+  
+      );
+    } else {
+      alert("no sing");
+      // No user is signed in.
+    }
+  });
+
 
 function writeToDatabase(catfile,contentfile,name,by,likes,place,tag){
   var newKey = firebase.database().ref('/post/').push();
@@ -111,14 +182,7 @@ function picarchive(Obj){
 }
 
 function piclike(Obj){
-  var user = firebase.auth().currentUser;
-  var akey= $(Obj).parent().attr("id");
-  //console.log(user.uid);
-  var newKey = firebase.database().ref('/user/'+user.uid+'/like/').push();
-  var akey= $(Obj).parent().attr("id");
-  newKey.set({
-    postnum: akey
-  });
+  
 }
 
 
@@ -156,3 +220,7 @@ function profiletest(){
   }
 }
 
+function alertTestFn(){
+  $('.toast').toast({delay: 700});
+  $('.toast').toast('show');
+}
