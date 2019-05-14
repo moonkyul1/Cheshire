f9139b4a6b9b2ab9006ab074cbb9ee57fb80bb8c
moonkyul1 <moonkyul1@kaist.ac.kr>
2019-05-15 01:54:46 +0900

    profile fund end

--- screen/profile/cat/catprofile.js
+++ screen/profile/cat/catprofile.js
@@ -182,24 +182,28 @@ function readFunding(catname) {
             //console.log("1");
             $('#prog1').attr('value',current);
             $('#prog1').attr('max',total);
+            $('#cost1').text('$'+total);
             $('#pbutton1').attr('disabled', false);
           }
           else if(myValue[currentKey].item=="BLANKET"){
             //console.log("2");
             $('#prog2').attr('value',current);
             $('#prog2').attr('max',total);
+            $('#cost2').text('$'+total);
             $('#pbutton2').attr('disabled', false);
           }
           else if(myValue[currentKey].item=="CAT TOY"){
             //console.log("3");
             $('#prog3').attr('value',current);
             $('#prog3').attr('max',total);
+            $('#cost3').text('$'+total);
             $('#pbutton3').attr('disabled', false);
           }
           else if(myValue[currentKey].item=="CAT HOUSE"){
             //console.log("4");
             $('#prog4').attr('value',current);
             $('#prog4').attr('max',total);
+            $('#cost4').text('$'+total);
             $('#pbutton4').attr('disabled', false);
           }
         }
@@ -225,8 +229,197 @@ function writefunding(Obj){
   });
 }
 
-
+function clickFunding1(catname) {
   
+  var a = document.getElementById( 'prog1' ).value;
+  document.getElementById( 'prog1' ).value = a*1 + 1;
+
+  var now= $('#prog1').attr('value');
+  var end=$('#prog1').attr('max');
+  /*
+     Read comments from the database
+     Print all the comments to the table
+  */
+  return firebase.database().ref('/cat/'+catname+'/project').once('value',function(snapshot){
+    var myValue = snapshot.val();
+    if(myValue==null){
+    
+    }
+    else{
+      var keyList = Object.keys(myValue);
+      for (var i=0;i<keyList.length;i++){
+        var currentKey = keyList[i];
+        if(myValue[currentKey].item=="CAT FOOD"){
+          if(myValue[currentKey].done==false){
+            var refDir='/cat/'+catname+'/project/'+currentKey+'/';
+            console.log(firebase.database().ref(refDir));
+            if(now==end){
+              firebase.database().ref(refDir).set({
+                item: "CAT FOOD",
+                total: end,
+                current: now,
+                done: true
+              });
+              $('#pbutton1').attr('disabled', true);
+            } 
+            else{
+              firebase.database().ref(refDir).set({
+                item: "CAT FOOD",
+                total: end,
+                current: now,
+                done: false
+              });
+            }
+          }
+        }
+      }
+    }
+  });
+}
+
+function clickFunding2(catname) {
+  
+  var a = document.getElementById( 'prog2' ).value;
+  document.getElementById( 'prog2' ).value = a*1 + 1;
+
+  var now= $('#prog2').attr('value');
+  var end=$('#prog2').attr('max');
+  /*
+     Read comments from the database
+     Print all the comments to the table
+  */
+  return firebase.database().ref('/cat/'+catname+'/project').once('value',function(snapshot){
+    var myValue = snapshot.val();
+    if(myValue==null){
+    
+    }
+    else{
+      var keyList = Object.keys(myValue);
+      for (var i=0;i<keyList.length;i++){
+        var currentKey = keyList[i];
+        if(myValue[currentKey].item=="BLANKET"){
+          if(myValue[currentKey].done==false){
+            var refDir='/cat/'+catname+'/project/'+currentKey+'/';
+            console.log(firebase.database().ref(refDir));
+            if(now==end){
+              firebase.database().ref(refDir).set({
+                item: "BLANKET",
+                total: end,
+                current: now,
+                done: true
+              });
+              $('#pbutton2').attr('disabled', true);
+            } 
+            else{
+              firebase.database().ref(refDir).set({
+                item: "BLANKET",
+                total: end,
+                current: now,
+                done: false
+              });
+            }
+          }
+        }
+      }
+    }
+  });
+}
+
+function clickFunding3(catname) {
+  
+  var a = document.getElementById( 'prog3' ).value;
+  document.getElementById( 'prog3' ).value = a*1 + 1;
+
+  var now= $('#prog3').attr('value');
+  var end=$('#prog3').attr('max');
+  /*
+     Read comments from the database
+     Print all the comments to the table
+  */
+  return firebase.database().ref('/cat/'+catname+'/project').once('value',function(snapshot){
+    var myValue = snapshot.val();
+    if(myValue==null){
+    
+    }
+    else{
+      var keyList = Object.keys(myValue);
+      for (var i=0;i<keyList.length;i++){
+        var currentKey = keyList[i];
+        if(myValue[currentKey].item=="CAT TOY"){
+          if(myValue[currentKey].done==false){
+            var refDir='/cat/'+catname+'/project/'+currentKey+'/';
+            console.log(firebase.database().ref(refDir));
+            if(now==end){
+              firebase.database().ref(refDir).set({
+                item: "CAT TOY",
+                total: end,
+                current: now,
+                done: true
+              });
+              $('#pbutton3').attr('disabled', true);
+            } 
+            else{
+              firebase.database().ref(refDir).set({
+                item: "CAT TOY",
+                total: end,
+                current: now,
+                done: false
+              });
+            }
+          }
+        }
+      }
+    }
+  });
+}
+
+function clickFunding4(catname) {
+  
+  var a = document.getElementById( 'prog4' ).value;
+  document.getElementById( 'prog4' ).value = a*1 + 1;
+
+  var now= $('#prog4').attr('value');
+  var end=$('#prog4').attr('max');
+  /*
+     Read comments from the database
+     Print all the comments to the table
+  */
+  return firebase.database().ref('/cat/'+catname+'/project').once('value',function(snapshot){
+    var myValue = snapshot.val();
+    if(myValue==null){
+    
+    }
+    else{
+      var keyList = Object.keys(myValue);
+      for (var i=0;i<keyList.length;i++){
+        var currentKey = keyList[i];
+        if(myValue[currentKey].item=="CAT HOUSE"){
+          if(myValue[currentKey].done==false){
+            var refDir='/cat/'+catname+'/project/'+currentKey+'/';
+            console.log(firebase.database().ref(refDir));
+            if(now==end){
+              firebase.database().ref(refDir).set({
+                item: "CAT HOUSE",
+                total: end,
+                current: now,
+                done: true
+              });
+              $('#pbutton4').attr('disabled', true);
+            } 
+            else{
+              firebase.database().ref(refDir).set({
+                item: "CAT HOUSE",
+                total: end,
+                current: now,
+                done: false
+              });
+            }
+          }
+        }
+      }
+    }
+  });
+}
 //readFromSave();
 
 
@@ -252,21 +445,17 @@ if (user) {
 });
 
 function JbFunc1() {
-  var a = document.getElementById( 'prog1' ).value;
-  document.getElementById( 'prog1' ).value = a*1 + 10;
+  clickFunding1(catname);
 }
 
 function JbFunc2() {
-  var a = document.getElementById( 'prog2' ).value;
-  document.getElementById( 'prog2' ).value = a*1 + 10;
+  clickFunding2(catname);
 }
 
 function JbFunc3() {
-  var a = document.getElementById( 'prog3' ).value;
-  document.getElementById( 'prog3' ).value = a*1 + 10;
+  clickFunding3(catname);
 }
 
 function JbFunc4() {
-  var a = document.getElementById( 'prog4' ).value;
-  document.getElementById( 'prog4' ).value = a*1 + 10;
+  clickFunding4(catname);
 }
